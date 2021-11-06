903b25b54e0df4b31ddd35bd80723f0f980a6cfd
javierpb275 <javierpb275@gmail.com>
2021-11-06 19:32:40 +0100

    Modules in python

---
+++ main.py
@@ -0,0 +1,6 @@
+import utility
+
+print(utility)  # <module 'utility' from 'C:\\Users\\javie\\Desktop\\programming-courses\\python-course\\python-modules\\utility.py'>
+
+utility.divide(4, 2)
+utility.multiply(2, 2)
